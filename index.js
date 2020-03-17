38dd2fba74a60bf1cbd4d9578d33a78816b209a3
Cloe Kouadjo <armelfli50@gmail.com>
2020-03-17 05:46:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much.";
-f2c9fcb35817de31cc311bd93dd2b571892163c2const token = 'YOUR_TOKEN_HERE';
+"const token ="f2c9fcb35817de31cc311bd93dd2b571892163c2" 'YOUR_TOKEN_HERE';
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
